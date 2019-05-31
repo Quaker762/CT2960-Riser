b53c5dff29e403d47316e56c772f81387a85757e
MatthewAtkin <s3603797@student.rmit.edu.au>
2019-06-01 00:58:08 +1000

    tcl timing

--- Verilog/src/tcl/SuperIO.tcl
+++ Verilog/src/tcl/SuperIO.tcl
@@ -4,12 +4,12 @@ proc writeBus {addressValue busValue} {
 force -freeze address 16#$addressValue
 force -freeze writedata 16#$busValue
 force -freeze write 1
-run 250000
+run 125000
 force -freeze write 0
 # Release the data and address buses.
 noforce writedata
 noforce address
-run 250000
+run 125000
 }
 
 # Create a function to read from an register to the data bus at
@@ -18,13 +18,13 @@ proc readBus {addressValue} {
 force -freeze address 16#$addressValue
 force -freeze write 0
 force -freeze read 1
-run 250000
-run 250000
+run 125000
+run 125000
 force -freeze read 0
 # Release the address bus.
 noforce readdata
 noforce address
-run 250000
+run 125000
 }
 
 proc runSim {} {
@@ -34,7 +34,7 @@ proc runSim {} {
     
     property wave -radix hex *
     
-    force -deposit clk_bus 1 0, 0 {125ns} -repeat 250000
+    force -deposit clk_bus 1 0, 0 {62500} -repeat 125000
     
     force -freeze global_reset 0
     run 2000
